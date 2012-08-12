fb9c2feb137d9c93ba3a4353ae855e25783b0915
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2012-08-13 00:04:56 +0200

    better handling of the idx stack

--- ADP/Fusion/MultiArg.hs
+++ ADP/Fusion/MultiArg.hs
@@ -18,56 +18,59 @@ import "PrimitiveArray" Data.Array.Repa.Shape
 import Text.Printf
 
 
-test1 = stack $ r
-test2 = mapM_ print . take 10 . S.toList $ mkStreamLast test1 (Z:.0:.3)
-
+test1 = stack $ b ~~ r ~~ r ~~ b
+test2 = mapM_ print . take 500 . S.toList $ mkStreamLast test1 (Z:.0:.6)
 
 class MkStream x where
   type ST x :: *
   mkStream :: x -> DIM2 -> S.Stream (ST x)
   mkStreamLast :: x -> DIM2 -> S.Stream (ST x)
 
+-- | the base of recursion leads to just Z:.i
+
 instance MkStream Z where
-  type ST Z = DIM2
-  mkStream Z ij = S.singleton ij
+  type ST Z = DIM1
+  mkStream Z (Z:.i:.j) = S.singleton (Z:.i)
   mkStreamLast = mkStream
 
-instance (MkStep x, MkStream y, ST y ~ Dim2 z) => MkStream (y :. x) where
+-- | each additional element adds one to the stack
+
+instance (MkStep x, MkStream y, ST y ~ Dim1 z) => MkStream (y :. x) where
   type ST (y :. x) = ST y :. Int
   mkStream (y :. x) ij@(Z:.i:.j) = S.flatten (mk x j) (step x j) Unknown $ mkStream y ij
   mkStreamLast (y :. x) ij@(Z:.i:.j) = case isConst x of
     Just c  -> S.flatten (mkLast x j) (stepLast x j) Unknown $ mkStreamLast y (Z:.i:.j-c)
     Nothing -> S.flatten (mkLast x j) (stepLast x j) Unknown $ mkStream y ij
 
-
 class MkStep x where
-  mk,mkLast :: x -> Int -> Dim2 z -> Dim3 z
-  step,stepLast :: x -> Int -> Dim3 z -> S.Step (Dim3 z) (Dim3 z)
+  mk,mkLast :: x -> Int -> Dim1 z -> Dim2 z
+  step,stepLast :: x -> Int -> Dim2 z -> S.Step (Dim2 z) (Dim2 z)
   isConst :: x -> Maybe Int
 
 instance MkStep Base where
-  mk _ j' (z:.i:.j) = (z:.i:.i+1:.j)
+  mk _ j (z:.i) = (z:.i:.i+1)
   mkLast = mk
-  step _ j' (z:.i:.k:.j)
-    | i+1==k    = S.Yield (z:.i:.k:.j) (z:.i:.j+1:.j)
-    | otherwise = S.Done
-  stepLast _ j' (z:.i:.k:.j)
-    | i+1==k && k==j' = S.Yield (z:.i:.k:.j) (z:.i:.j'+1:.j)
+  step _ j (z:.i:.k)
+    | i+1==k && k <=j = S.Yield (z:.i:.k) (z:.i:.j+1)
     | otherwise       = S.Done
+  stepLast _ j (z:.i:.k)
+    | i+1==k && k==j = S.Yield (z:.i:.k) (z:.i:.j+1)
+    | otherwise      = S.Done
   isConst _ = Just 1
 
 instance MkStep Region where
-  mk _ j' (z:.i:.j) = (z:.i:.i+1:.j)
-  mkLast = mk
-  step _ j' (z:.i:.k:.j)
-    | k+1<=j    = S.Yield (z:.i:.k+1:.j) (z:.i:.k+1:.j)
+  mk _ j (z:.i) = (z:.i:.i+1)
+  mkLast _ j (z:.i) = let k = max (i+1) j in (z:.i:.k)
+  step _ j (z:.i:.k)
+    | k<=j    = S.Yield (z:.i:.k) (z:.i:.k+1)
     | otherwise = S.Done
-  stepLast _ j' (z:.i:.k:.j)
-    | k+1<=j'   = S.Yield (z:.i:.k+1:.j) (z:.i:.k+1:.j)
+  stepLast _ j (z:.i:.k)
+    | k<=j    = S.Yield (z:.i:.k) (z:.i:.k+1)
     | otherwise = S.Done
   isConst _ = Nothing
 
-type Dim2 z = (z:.Int:.Int)
+type Dim1 z = z:.Int
+type Dim2 z = Dim1 z :. Int
 type Dim3 z = Dim2 z :. Int
 
 
