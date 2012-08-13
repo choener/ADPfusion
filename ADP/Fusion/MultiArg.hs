32469602746efad2be94749fe3bdfb8b41b18a50
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2012-08-13 22:50:33 +0200

    simple size dependencies added
    It is actually not quite trivial to allow for linked regions and other
    fun if you want to let the compiler figure out what to do.
    
    The solution, for now, is to have constraint kinds, and allow for
    arbitrary constraints based on type classes.
    
    At least for the smallish example I am using now, this leads to
    efficient code (funnily enough). Now blowing up to something real ...

--- ADP/Fusion/MultiArg.hs
+++ ADP/Fusion/MultiArg.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE PackageImports #-}
 {-# LANGUAGE TypeOperators #-}
@@ -16,58 +18,76 @@ import Data.Vector.Fusion.Stream.Size
 import "PrimitiveArray" Data.Array.Repa.Index
 import "PrimitiveArray" Data.Array.Repa.Shape
 import Text.Printf
+import GHC.Prim (Constraint(..))
+import Debug.Trace
 
 
 test1 = stack $ b ~~ r ~~ r ~~ b
-test2 = mapM_ print . take 500 . S.toList $ mkStreamLast test1 (Z:.0:.6)
+--test2 = mapM_ print . take 500 . S.toList $ mkStream test1 (Z:.0:.6)
+test2 = S.length $ mkStream test1 (Z:.0:.6)
 
 class MkStream x where
+  type XX x :: *
   type ST x :: *
-  mkStream :: x -> DIM2 -> S.Stream (ST x)
-  mkStreamLast :: x -> DIM2 -> S.Stream (ST x)
-
--- | the base of recursion leads to just Z:.i
+  type SC x :: Constraint
+  type SC x = ()
+  mkStream :: SC x => x -> DIM2 -> S.Stream (ST x)
+  mk   :: SC x => x -> Int -> XX x -> ST x
+  step :: SC x => x -> Int -> ST x -> S.Step (ST x) (ST x)
+  mk _ _ _ = undefined
+  step _ _ _ = undefined
+  {-# INLINE mk #-}
+  {-# INLINE step #-}
 
 instance MkStream Z where
+  type XX Z = Z
   type ST Z = DIM1
   mkStream Z (Z:.i:.j) = S.singleton (Z:.i)
-  mkStreamLast = mkStream
+  {-# INLINE mkStream #-}
 
--- | each additional element adds one to the stack
-
-instance (MkStep x, MkStream y, ST y ~ Dim1 z) => MkStream (y :. x) where
-  type ST (y :. x) = ST y :. Int
-  mkStream (y :. x) ij@(Z:.i:.j) = S.flatten (mk x j) (step x j) Unknown $ mkStream y ij
-  mkStreamLast (y :. x) ij@(Z:.i:.j) = case isConst x of
-    Just c  -> S.flatten (mkLast x j) (stepLast x j) Unknown $ mkStreamLast y (Z:.i:.j-c)
-    Nothing -> S.flatten (mkLast x j) (stepLast x j) Unknown $ mkStream y ij
-
-class MkStep x where
-  mk,mkLast :: x -> Int -> Dim1 z -> Dim2 z
-  step,stepLast :: x -> Int -> Dim2 z -> S.Step (Dim2 z) (Dim2 z)
-  isConst :: x -> Maybe Int
-
-instance MkStep Base where
-  mk _ j (z:.i) = (z:.i:.i+1)
-  mkLast = mk
-  step _ j (z:.i:.k)
+instance (MkStream y, SC y, ST y ~ Dim1 z) => MkStream (y :. Base) where
+  type XX (y :. Base) = ST y
+  type ST (y :. Base) = ST y :. Int
+  type SC (y:.Base) = ()
+  mkStream (y :. x) ij@(Z:.i:.j) = S.flatten (mk (y:.x) j) (step (y:.x) j) Unknown $ mkStream y ij
+  mk yx j (z:.i) = (z:.i:.i+1)
+  step yx j (z:.i:.k)
     | i+1==k && k <=j = S.Yield (z:.i:.k) (z:.i:.j+1)
     | otherwise       = S.Done
-  stepLast _ j (z:.i:.k)
-    | i+1==k && k==j = S.Yield (z:.i:.k) (z:.i:.j+1)
-    | otherwise      = S.Done
-  isConst _ = Just 1
+  {-# INLINE mkStream #-}
+  {-# INLINE mk #-}
+  {-# INLINE step #-}
 
-instance MkStep Region where
-  mk _ j (z:.i) = (z:.i:.i+1)
-  mkLast _ j (z:.i) = let k = max (i+1) j in (z:.i:.k)
-  step _ j (z:.i:.k)
-    | k<=j    = S.Yield (z:.i:.k) (z:.i:.k+1)
-    | otherwise = S.Done
-  stepLast _ j (z:.i:.k)
-    | k<=j    = S.Yield (z:.i:.k) (z:.i:.k+1)
-    | otherwise = S.Done
-  isConst _ = Nothing
+instance (MkStream y, SC y, ST y ~ Dim1 z) => MkStream (y :. Region) where
+  type XX (y :. Region) = ST y
+  type ST (y :. Region) = ST y :. Int
+  type SC (y:.Region) = Deep (y:.Region)
+  mkStream (y :. x) ij@(Z:.i:.j) = S.flatten (mk (y:.x) j) (step (y:.x) j) Unknown $ mkStream y ij
+  mk yx j (z:.i) = (z:.i:.i+1) where
+    g = get yx (z:.i:.i+1)
+  step yx j (z:.i:.k)
+    | i+1<=k && k <=j && g<=2 = {- trace (printf "g: %d\n" g) $ -} S.Yield (z:.i:.k) (z:.i:.k+1)
+    | otherwise       = S.Done
+    where g = tot yx (z:.i:.i+1)
+  {-# INLINE mkStream #-}
+  {-# INLINE mk #-}
+  {-# INLINE step #-}
+
+class Deep x where
+  get :: x -> ST x -> Int
+  tot :: x -> ST x -> Int
+
+instance Deep Z where
+  get _ (Z:.i) = i
+  tot _ _ = 0
+
+instance Deep z => Deep (z:.Region) where
+  get _ (_:.i) = i
+  tot (z:._) (ii:.j) = tot z ii + (j - get z ii)
+
+instance Deep z => Deep (z:.Base) where
+  get _ (_:.i) = i
+  tot (z:._) (ii:._) = tot z ii
 
 type Dim1 z = z:.Int
 type Dim2 z = Dim1 z :. Int
@@ -75,13 +95,20 @@ type Dim3 z = Dim2 z :. Int
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 infixr ~~
 (~~) = (,)
 
-infixr :~~
-data (:~~) a b = a :~~ b deriving (Show)
-
-data Y a = Y a deriving (Show)
 
 
 class MkStack x where
@@ -89,16 +116,16 @@ class MkStack x where
   stack :: x -> Stack x
 
 instance MkStack Base where
-  type Stack Base = Z :. Base -- Base :~~ Z
-  stack b = Z :. b -- b :~~ Z
+  type Stack Base = Z :. Base
+  stack b = Z :. b
 
 instance MkStack Region where
   type Stack Region = Z :. Region
   stack r = Z :. r
 
 instance (MkStack y) => MkStack (x,y) where
-  type Stack (x,y) = Stack y :. x -- x :~~ Stack y
-  stack (x,y) = stack y :. x -- x :~~ stack y
+  type Stack (x,y) = Stack y :. x
+  stack (x,y) = stack y :. x
 
 data Base = Base [Char]
   deriving (Show)
@@ -109,6 +136,3 @@ data Region = Region [Char]
 
 r = Region ['a'..'z']
 
-
--- iloop <<< bp ~~ r30 ~~ pb ~~ stem ~~ bp ~~ r30 ~~ pb
--- mloop <<< bp ~~ m1 ~~ m ~~ pb
