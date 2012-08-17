49c6c5a50b9ca6c9afa19ed201be2e1932541a90
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2012-08-17 18:52:22 +0200

    playing with mutable vectors

--- ADP/Fusion/GAPlike.hs
+++ ADP/Fusion/GAPlike.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE ConstraintKinds #-}
@@ -23,16 +24,19 @@ import Text.Printf
 import GHC.Prim (Constraint(..))
 import Debug.Trace
 import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VUM
 import ADP.Fusion.Monadic.Internal (Apply(..))
 import Data.Char
+import Control.Monad.ST
 
-type family ST x :: *
-type instance ST Z   = (W Z  , Int)
-type instance ST Chr = (W Chr, Int)
+type family STtop x :: *
+type instance STtop Z   = (W Z  , Int)
+type instance STtop Chr = (W Chr, Int)
+type instance STtop (VUM.MVector s elm) = (W (VUM.MVector s elm), Int)
 
 type family Stack x :: *
-type instance Stack Z = Z:.ST Z
-type instance Stack (x:.y) = Stack x :. ST y
+type instance Stack Z = Z:.STtop Z
+type instance Stack (x:.y) = Stack x :. STtop y
 
 data W t = W
 
@@ -52,7 +56,18 @@ instance (Monad m) => MkStream m Z where
 instance (Monad m, MkStream m x, SC x) => MkStream m (x:.Chr) where
   type SC (x:.Chr) = (Get (Stack x))
   mkStream (x:.Chr cs) (Z:.i:.j) = S.flatten mk step Unknown $ mkStream x (Z:.i:.j) where
-    mk z = z `seq` return $ z:.(W, get z)
+    mk z = return $ z:.(W, get z)
+    step zj@(z:.(w, k))
+      | k<=j      = return $ S.Yield zj (z:.(w,(j+1)))
+      | otherwise = return $ S.Done
+    {-# INLINE mk #-}
+    {-# INLINE step #-}
+  {-# INLINE mkStream #-}
+
+instance (Monad m, MkStream m x, SC x) => MkStream m (x:.VUM.MVector s elm) where
+  type SC (x:.VUM.MVector s elm) = (Get (Stack x))
+  mkStream (x:.mvec) (Z:.i:.j) = S.flatten mk step Unknown $ mkStream x (Z:.i:.j) where
+    mk z = return $ z:.(W, get z)
     step zj@(z:.(w, k))
       | k<=j      = return $ S.Yield zj (z:.(w,(j+1)))
       | otherwise = return $ S.Done
@@ -73,6 +88,18 @@ instance  Get (x:.(W y, Int)) where
 
 testZ i j = SPure.length $ mkStream (Z:.ccc) (Z:.i:.j)
 {-# NOINLINE testZ #-}
+
+testST :: Int -> Int -> Int
+testST i j = runST $ embedST i j
+{-# NOINLINE testST #-}
+
+embedST :: Int -> Int -> ST s Int
+embedST i j = do
+  vm :: VUM.MVector s Int <- VUM.replicate 10 0
+  v <- S.length $ mkStream (Z:.ccc:.vm) (Z:.i:.j)
+  return v
+{-# INLINE embedST #-}
+
 
 
 data Chr = Chr (VU.Vector Char)
