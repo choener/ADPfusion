784ab4576f1254cd43212af5ec292f08f9ef8717
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2012-08-28 22:46:45 +0200

    working on GAPlike2

--- ADP/Fusion/GAPlike2.hs
+++ ADP/Fusion/GAPlike2.hs
@@ -1,3 +1,4 @@
+{- LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE TypeOperators #-}
@@ -16,107 +17,154 @@ import Control.Monad.ST
 import Data.Primitive
 import Data.PrimitiveArray
 import Data.PrimitiveArray.Unboxed.Zero as UZ
+import Data.PrimitiveArray.Unboxed.VectorZero as UVZ
 import Data.Vector.Fusion.Stream.Size
 import GHC.Prim (Constraint)
 import "PrimitiveArray" Data.Array.Repa.Index
 import "PrimitiveArray" Data.Array.Repa.Shape
 import qualified Data.Vector.Fusion.Stream.Monadic as S
+import qualified Data.Vector.Fusion.Stream as SP
 import qualified Data.Vector.Unboxed as VU
 import Text.Printf
 import Data.List (intersperse)
+import Data.Char
 
 
 
-class MkStream m x where
+
+class StreamElement x where
+  -- | one element of the stream, recursively defined
+  data StreamElm x :: *
+  -- | top-most index of the stream -- typically int
+  type StreamTopIdx  x :: *
+  -- | complete, recursively defined argument of the stream
+  type StreamArg  x :: *
+  -- | Given a stream element, we extract the top-most idx
+  getTopIdx :: StreamElm x -> StreamTopIdx x
+  -- | extract the recursively defined argument in a well-defined way for 'apply'
+  getArg :: StreamElm x -> StreamArg x
+
+class (StreamConstraint x) => MkStream m x where
   type StreamConstraint x :: Constraint
   type StreamConstraint x = ()
-  type StreamType x :: *
-  mkStream      :: (StreamConstraint x) => x -> (Int,Int) -> S.Stream m (StreamType x)
-  mkStreamInner :: (StreamConstraint x) => x -> (Int,Int) -> S.Stream m (StreamType x)
+  mkStream      :: (StreamConstraint x) => x -> (Int,Int) -> S.Stream m (StreamElm x)
+  mkStreamInner :: (StreamConstraint x) => x -> (Int,Int) -> S.Stream m (StreamElm x)
 
 
 
-data ArgBottom = ArgBottom
-  deriving (Show)
+-- * Default instances of StreamElement / MkStream
 
-data AB = AB
-  deriving (Show)
+-- ** Terminates the stack of arguments
 
-instance (Monad m) => MkStream m ArgBottom where
-  type StreamType ArgBottom = AB:.(Int,())
-  mkStream = mkStreamInner
-  mkStreamInner ArgBottom (i,j) = S.singleton (AB:.(i,()))
+-- | Very simple data ctor
+
+data None = None
+
+-- | For CORE-language, we have our own Arg-terminator
+
+data ArgZ = ArgZ
+
+instance StreamElement None where
+  data StreamElm None    = SeNone !Int
+  type StreamTopIdx None = Int
+  type StreamArg    None = ArgZ
+  getTopIdx (SeNone k) = k
+  getArg _ = ArgZ
+  {-# INLINE getTopIdx #-}
+  {-# INLINE getArg #-}
+
+instance (Monad m) => MkStream m None where
+  mkStream None (i,j) = S.unfoldr step i where
+    step k
+      | k<=j = Just (SeNone i, j+1)
+      | otherwise = Nothing
+    {-# INLINE step #-}
   {-# INLINE mkStream #-}
+  mkStreamInner = mkStream
   {-# INLINE mkStreamInner #-}
 
+-- ** A single character terminal. Using unboxed vector to hold the input.
 
-
-data Chr e = Chr (VU.Vector e)
-  deriving (Show)
+data Chr e = Chr !(VU.Vector e)
 
 instance Build (Chr e) where
-  type BuildStack (Chr e) = ArgBottom:.Chr e
-  build c = ArgBottom:.c
+  type BuildStack (Chr e) = None:.(Chr e)
+  build c = None:.c
   {-# INLINE build #-}
 
-instance
-  ( Monad m
-  , MkStream m x
-  , StreamConstraint x
-  , StreamType x ~ (t0:.(Int,h0))
-  , VU.Unbox e
-  ) => MkStream m (x:.Chr e) where
-  type StreamType (x:.Chr e) = StreamType x :. (Int,e)
-  mkStream (x:.Chr cs) (i,j) = S.flatten mk step Unknown $ mkStream x (i,j-1) where
-    mk (z:.(k,ka)) = return (z:.(k,ka):.j)
-    step (z:.(k,ka):.l)
-      | k+1==l && l==j = let c = VU.unsafeIndex cs k in c `seq` return $ S.Yield (z:.(k,ka):.(l,c)) (z:.(k,ka):.(j+1))
-      | otherwise      = return $ S.Done
-    {-# INLINE mk #-}
-    {-# INLINE step #-}
-  mkStreamInner (x:.Chr cs) (i,j) = S.flatten mk step Unknown $ mkStreamInner x (i,j) where
-    mk (z:.(k,ka)) = return (z:.(k,ka):.k+1)
-    step (z:.(k,ka):.l)
-      | l<=j      = let c = VU.unsafeIndex cs k in c `seq` return $ S.Yield (z:.(k,ka):.(l,c)) (z:.(k,ka):.l+1)
-      | otherwise = return $ S.Done
-  {-# INLINE mkStream #-}
-  {-# INLINE mkStreamInner #-}
+instance (StreamElement x) => StreamElement (x:.Chr e) where
+  data StreamElm (x:.Chr e) = SeChr !(StreamElm x) !Int !e
+  type StreamTopIdx (x:.Chr e) = Int
+  type StreamArg (x:.Chr e) = StreamArg x :. e
+  getTopIdx (SeChr _ k _) = k
+  getArg (SeChr x _ e) = getArg x :. e
+  {-# INLINE getTopIdx #-}
+  {-# INLINE getArg #-}
 
-
-
-instance Build (UZ.MArr0 s DIM2 e) where
-  type BuildStack (UZ.MArr0 s DIM2 e) = ArgBottom:.MArr0 s DIM2 e
-  build c = ArgBottom:.c
-  {-# INLINE build #-}
-
-instance
-  ( Monad m
-  , PrimMonad m
-  , PrimState m ~ s
-  , MkStream m x
-  , StreamConstraint x
-  , Prim e
-  , StreamType x ~ (t0:.(Int,h0))
-  ) => MkStream m (x:. UZ.MArr0 s DIM2 e) where
-  type StreamType (x:. UZ.MArr0 s DIM2 e) = StreamType x :. (Int,e)
-  mkStream (x:.pa) (i,j) = S.flatten mk step Unknown $ mkStreamInner x (i,j) where
-    mk (z:.(k,ka)) = return (z:.(k,ka):.j)
-    step (z:.(k,ka):.l)
-      | k<l && l<=j = readM pa (Z:.k:.l) >>= \c -> c `seq` return $ S.Yield (z:.(k,ka):.(l,c)) (z:.(k,ka):.j+1)
-      | otherwise   = return $ S.Done
-    {-# INLINE mk #-}
-    {-# INLINE step #-}
-  mkStreamInner (x:.pa) (i,j) = S.flatten mk step Unknown $ mkStreamInner x (i,j) where
-    mk (z:.(k,ka)) = return (z:.(k,ka):.k+1)
-    step (z:.(k,ka):.l)
-      | l<=j      = readM pa (Z:.k:.l) >>= \c -> c `seq` return $ S.Yield (z:.(k,ka):.(l,c)) (z:.(k,ka):.l+1)
-      | otherwise = return $ S.Done
+instance (Monad m, MkStream m x, StreamElement x, StreamTopIdx x ~ Int, VU.Unbox e) => MkStream m (x:.Chr e) where
+  mkStream (x:.Chr es) (i,j) = S.flatten mk step Unknown $ mkStream x (i,j-1) where
+    mk x = return (x, getTopIdx x)
+    step (x,k)
+      | k+1 == j = return $ S.Yield (SeChr x (k+1) (VU.unsafeIndex es k)) (x,j+1)
+      | otherwise = return S.Done
     {-# INLINE mk #-}
     {-# INLINE step #-}
   {-# INLINE mkStream #-}
+  mkStreamInner (x:.Chr es) (i,j) = S.flatten mk step Unknown $ mkStreamInner x (i,j-1) where
+    mk x = return (x, getTopIdx x)
+    step (x,k)
+      | k < j     = return $ S.Yield (SeChr x (k+1) (VU.unsafeIndex es k)) (x,j+1)
+      | otherwise = return $ S.Done
+    {-# INLINE mk #-}
+    {-# INLINE step #-}
   {-# INLINE mkStreamInner #-}
 
+-- ** Non-empty two-dimensional subwords, for non-terminal use
 
+data NonEmptyTbl s e = NonEmptyTbl !(UVZ.MArr0 s DIM2 e)
+
+instance Build (NonEmptyTbl s e) where
+  type BuildStack (NonEmptyTbl s e) = None:.NonEmptyTbl s e
+  build t = None:.t
+  {-# INLINE build #-}
+
+instance (StreamElement x) => StreamElement (x:.NonEmptyTbl s e) where
+  data StreamElm (x:.NonEmptyTbl s e) = SeNonEmptyTbl !(StreamElm x) !Int !e
+  type StreamTopIdx (x:.NonEmptyTbl s e) = Int
+  type StreamArg (x:.NonEmptyTbl s e) = StreamArg x :. e
+  getTopIdx (SeNonEmptyTbl _ k _) = k
+  getArg (SeNonEmptyTbl x _ e) = getArg x :. e
+  {-# INLINE getTopIdx #-}
+  {-# INLINE getArg #-}
+
+instance (Monad m, PrimMonad m, PrimState m ~ s, MkStream m x, StreamElement x, StreamTopIdx x ~ Int, VU.Unbox e) => MkStream m (x:.NonEmptyTbl s e) where
+  {-
+  mkStream (x:.NonEmptyTbl t) (i,j) = S.flatten mk step Unknown $ mkStreamInner x (i,j-1) where
+    mk :: StreamElm x -> m (StreamElm x, Int)
+    mk x = return (x,getTopIdx x)
+    step :: (StreamElm x, Int) -> m (S.Step (StreamElm x, Int) (StreamElm (x:.NonEmptyTbl s e)))
+    step (x,k)
+      | k < j = readM t (Z:.k:.j) >>= \e -> return $ S.Yield (SeNonEmptyTbl x k e) (x,j+1)
+      | otherwise = return S.Done
+    {-# INLINE mk #-}
+    {-# INLINE step #-}
+  -}
+  mkStream (x:.NonEmptyTbl t) (i,j) = S.mapM step $ mkStreamInner x (i,j-1) where
+    step :: StreamElm x -> m (StreamElm (x:.NonEmptyTbl s e))
+    step x = let k = getTopIdx x in readM t (Z:.k:.j) >>= \e -> return $ SeNonEmptyTbl x k e
+    {-# INLINE step #-}
+  {-# INLINE mkStream #-}
+  mkStreamInner (x:.NonEmptyTbl t) (i,j) = S.flatten mk step Unknown $ mkStreamInner x (i,j-1) where
+    mk :: StreamElm x -> m (StreamElm x, Int)
+    mk x = return (x,getTopIdx x + 1)
+    step :: (StreamElm x, Int) -> m (S.Step (StreamElm x, Int) (StreamElm (x:.NonEmptyTbl s e)))
+    step (x,l)
+      | k<l && l <= j = readM t (Z:.k:.l) >>= \e -> return $ S.Yield (SeNonEmptyTbl x l e) (x,l+1)
+      | otherwise = return S.Done
+      where k = getTopIdx x
+    {-# INLINE mk #-}
+    {-# INLINE step #-}
+  {-# INLINE mkStreamInner #-}
 
 -- * Build
 
@@ -134,7 +182,7 @@ instance Build x => Build (x,y) where
 -- * combinators
 
 infixl 8 <<<
-(<<<) f t ij = S.map (\(_,za) -> apply f za) $ mkStream (build t) ij
+(<<<) f t ij = S.map (\s -> apply f $ getArg s) $ mkStream (build t) ij
 {-# INLINE (<<<) #-}
 
 infixl 7 |||
@@ -155,33 +203,6 @@ infixl 9 %
 
 
 
--- * Apply
-
-class Apply x where
-  type Fun x :: *
-  apply :: Fun x -> x
-
-
-instance Apply (AB:.(Int,())) where
-  Fun (f -> (AB:.(Int,()))) = (f -> f)
-  apply f _ = f
-
-instance Apply x => Apply (x:.(Int,y)) where
-  apply f (xs:.(_,y)) = (apply f xs) y
-
-{-
-mkApply :: Char -> IO ()
-mkApply to = do
-  let xs = ['a' .. to]
-  let zs :: String = concatMap (printf ":.(Int,%c)") xs
-  let as :: String = concatMap (printf "%c -> ") $ xs
-  let fs :: String = concatMap (printf "%c ") $ xs
-  printf "instance Apply (AB:.(Int,())%s -> res) where\n" zs
-  printf "  type  Fun (AB:.(Int,())%s -> res) = %sres\n" zs as
-  printf "  apply fun (_:._%s) = fun %s\n" zs fs
-  printf "  {-# INLINE apply #-}\n"
--}
-
 
 
 
@@ -191,6 +212,100 @@ mkApply to = do
 
 -- * Criterion tests
 
+testC :: Int -> Int -> Int
+testC i j = runST doST where
+  doST :: ST s Int
+  doST = do
+    let c = Chr dvu
+    (gord1 <<< c ... ghsum) (i,j)
+{-# NOINLINE testC #-}
+
+gTestC (ord1,hsum) c =
+  (ord1 <<< c ... hsum)
+
+aTestC = (ord1,hsum) where
+  ord1 = gord1
+  hsum = ghsum
+
+testCC :: Int -> Int -> Int
+testCC i j = runST doST where
+  doST :: ST s Int
+  doST = do
+    let c = Chr dvu
+    let d = Chr dvu
+    (gord2 <<< c % d ... ghsum) (i,j)
+{-# NOINLINE testCC #-}
+
+testT :: Int -> Int -> Int
+testT i j = runST doST where
+  doST :: ST s Int
+  doST = do
+    tbl <- NonEmptyTbl `fmap` fromAssocsM (Z:.0:.0) (Z:.j:.j) 1 []
+    (id <<< tbl ... ghsum) (i,j)
+{-# NOINLINE testT #-}
+
+testTT :: Int -> Int -> Int
+testTT i j = runST doST where
+  doST :: ST s Int
+  doST = do
+    tbl <- NonEmptyTbl `fmap` fromAssocsM (Z:.0:.0) (Z:.j:.j) 1 []
+    (gplus2 <<< tbl % tbl ... ghsum) (i,j)
+  {-# INLINE doST #-}
+{-# NOINLINE testTT #-}
+
+testTTT :: Int -> Int -> Int
+testTTT i j = runST doST where
+  doST :: ST s Int
+  doST = do
+    tbl <- NonEmptyTbl `fmap` fromAssocsM (Z:.0:.0) (Z:.j:.j) 1 []
+    (gplus3 <<< tbl % tbl % tbl ... ghsum) (i,j)
+{-# NOINLINE testTTT #-}
+
+testTTTT :: Int -> Int -> Int
+testTTTT i j = runST doST where
+  doST :: ST s Int
+  doST = do
+    tbl <- NonEmptyTbl `fmap` fromAssocsM (Z:.0:.0) (Z:.j:.j) (1::Int) []
+    (gplus4 <<< tbl % tbl % tbl % tbl ... ghsum) (i,j)
+  {-# INLINE doST #-}
+{-# NOINLINE testTTTT #-}
+
+testTTTTga :: Int -> Int -> Int
+testTTTTga i j = runST doST where
+  doST :: ST s Int
+  doST = do
+    tbl <- NonEmptyTbl `fmap` fromAssocsM (Z:.0:.0) (Z:.j:.j) (1::Int) []
+    -- (gplus4 <<< tbl % tbl % tbl % tbl ... ghsum) (i,j)
+    gTTTga aTTTga tbl (i,j)
+  {-# INLINE doST #-}
+{-# NOINLINE testTTTTga #-}
+
+gTTTga (plus4, hsum) tbl =
+  (plus4 <<< tbl % tbl % tbl % tbl ... hsum)
+
+aTTTga = (plus4, hsum) where
+  plus4 = gplus4
+  hsum = ghsum
+
+gord1 a = ord a
+
+gord2 a b = ord a + ord b
+
+gord3 a b c = ord a + ord b + ord c
+
+gplus2 a b = a+b
+
+gplus3 a b c = a+b+c
+
+gplus4 a b c d = a+b+c+d
+
+ghsum :: S.Stream (ST s) Int -> ST s Int
+ghsum = S.foldl' (+) 0
+
+dvu = VU.fromList $ concat $ replicate 10 ['a'..'z']
+{-# NOINLINE dvu #-}
+
+{-
 testM3 :: Int -> Int -> Int
 testM3 i j = runST doST where
   doST :: ST s Int
@@ -208,4 +323,84 @@ testM4 i j = runST doST where
     S.length $ mkStream (ArgBottom:.s:.s:.s:.s) (i,j)
   {-# INLINE doST #-}
 {-# NOINLINE testM4 #-}
+-}
+
+class Apply x where
+  type Fun x :: *
+  apply :: Fun x -> x
+
+instance Apply (ArgZ:.a -> res) where
+  type Fun (ArgZ:.a -> res) = a -> res
+  apply fun (ArgZ:.a) = fun a
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b -> res) where
+  type Fun (ArgZ:.a:.b -> res) = a->b -> res
+  apply fun (ArgZ:.a:.b) = fun a b
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c -> res) where
+  type Fun (ArgZ:.a:.b:.c -> res) = a->b->c -> res
+  apply fun (ArgZ:.a:.b:.c) = fun a b c
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d -> res) = a->b->c->d -> res
+  apply fun (ArgZ:.a:.b:.c:.d) = fun a b c d
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d:.e -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d:.e -> res) = a->b->c->d->e -> res
+  apply fun (ArgZ:.a:.b:.c:.d:.e) = fun a b c d e
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d:.e:.f -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d:.e:.f -> res) = a->b->c->d->e->f -> res
+  apply fun (ArgZ:.a:.b:.c:.d:.e:.f) = fun a b c d e f
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d:.e:.f:.g -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d:.e:.f:.g -> res) = a->b->c->d->e->f->g -> res
+  apply fun (ArgZ:.a:.b:.c:.d:.e:.f:.g) = fun a b c d e f g
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h -> res) = a->b->c->d->e->f->g->h -> res
+  apply fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h) = fun a b c d e f g h
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i -> res) = a->b->c->d->e->f->g->h->i -> res
+  apply fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i) = fun a b c d e f g h i
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j -> res) = a->b->c->d->e->f->g->h->i->j -> res
+  apply fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j) = fun a b c d e f g h i j
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k -> res) = a->b->c->d->e->f->g->h->i->j->k -> res
+  apply fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k) = fun a b c d e f g h i j k
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l -> res) = a->b->c->d->e->f->g->h->i->j->k->l -> res
+  apply fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l) = fun a b c d e f g h i j k l
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l:.m -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l:.m -> res) = a->b->c->d->e->f->g->h->i->j->k->l->m -> res
+  apply fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l:.m) = fun a b c d e f g h i j k l m
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l:.m:.n -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l:.m:.n -> res) = a->b->c->d->e->f->g->h->i->j->k->l->m->n -> res
+  apply fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l:.m:.n) = fun a b c d e f g h i j k l m n
+  {-# INLINE apply #-}
+
+instance Apply (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l:.m:.n:.o -> res) where
+  type Fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l:.m:.n:.o -> res) = a->b->c->d->e->f->g->h->i->j->k->l->m->n->o -> res
+  apply fun (ArgZ:.a:.b:.c:.d:.e:.f:.g:.h:.i:.j:.k:.l:.m:.n:.o) = fun a b c d e f g h i j k l m n o
+  {-# INLINE apply #-}
 
--- GAPcriterion.hs
+++ GAPcriterion.hs
@@ -8,6 +8,24 @@ import ADP.Fusion.GAPlike2
 
 
 main = defaultMain
+  [ bgroup "testTTT3"
+    [ bench "  10" (whnf (testTTT 0)   10)
+    , bench " 100" (whnf (testTTT 0)  100)
+    , bench "1000" (whnf (testTTT 0) 1000)
+    ]
+  , bgroup "testTTTT4"
+    [ bench "  10" (whnf (testTTTT 0)   10)
+    , bench " 100" (whnf (testTTTT 0)  100)
+    , bench "1000" (whnf (testTTTT 0) 1000)
+    ]
+  , bgroup "testTTTT4ga"
+    [ bench "  10" (whnf (testTTTTga 0)   10)
+    , bench " 100" (whnf (testTTTTga 0)  100)
+    , bench "1000" (whnf (testTTTTga 0) 1000)
+    ]
+  ]
+{-
+main = defaultMain
   [ bgroup "three tables, O(n^2)"
     [ bench "   10" (whnf (testM3 0)    10)
     , bench "  100" (whnf (testM3 0)   100)
@@ -19,4 +37,4 @@ main = defaultMain
     , bench " 1000" (whnf (testM4 0)  1000)
     ]
   ]
-
+-}
