f44fde6c467704c5119ce52bd687ef62e818b20b
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-05-25 16:05:48 +0200

    multi-tape fusion try-outs
    this version of the multi-tape system reduces size-1 loops thereby
    increasing speed

--- ADP/Fusion/Multi.hs
+++ ADP/Fusion/Multi.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE UnicodeSyntax #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -11,8 +15,12 @@ module ADP.Fusion.Multi where
 
 import Data.Array.Repa.Index
 import Data.Strict.Tuple
+import qualified Data.Vector.Fusion.Stream.Monadic as S
+
+import Data.Array.Repa.Index.Subword
 
 import ADP.Fusion.Classes
+import ADP.Fusion.Chr (GChr (..))
 
 -- import Data.Array.Repa.Index.Subword
 
@@ -34,10 +42,55 @@ instance
   validIndex (ls :!: t) abc ix =
     allDimensionsValid t abc ix && validIndex ls abc ix
   {-# INLINE validIndex #-}
-  getParserRange (ls :!: t) ix = undefined
+  getParserRange (ls :!: t) ix = updateRange t ix (getParserRange ls ix)
+  {-# INLINE getParserRange #-}
 
+allDimensionsValid = undefined
+updateRange = undefined
 
+instance
+  ( Elms ls ix
+  ) => Elms (ls :!: Term a b) ix where
+    data Elm (ls :!: Term a b) ix = ElmTerm !(Elm ls ix) !(TermOf (Term a b)) !ix
+    type Arg (ls :!: Term a b) = Arg ls :. (TermOf (Term a b))
+    getArg !(ElmTerm ls x _) = getArg ls :. x
+    getIdx !(ElmTerm _ _ idx) = idx
+    {-# INLINE getArg #-}
+    {-# INLINE getIdx #-}
 
+instance
+  ( Monad m
+  , Elms ls ix
+  , MkStream m ls ix
+  , TermElm m (Term a b) ix
+  ) => MkStream m (ls :!: Term a b) ix where
+  mkStream !(ls :!: t) !io !ij
+    = S.map (\(s:!:Z:!:zij:!:e) -> ElmTerm s e zij)
+    $ termStream t io ij
+    $ S.map (\s -> (s :!: Z :!: getIdx s))
+    $ mkStream ls (leftTermIO io) (leftTermIndex ij)
+  {-# INLINE mkStream #-}
+
+leftTermIndex = id
+leftTermIO = id
+
+class
+  ( Monad m
+  ) => TermElm m t ix where
+  type TermOf t :: *
+  termStream :: t -> InOut ix -> ix -> S.Stream m (ze :!: zix :!: ix) -> S.Stream m (ze :!: zix :!: ix :!: TermOf t)
+
+instance
+  ( Monad m
+  , TermElm m ts is
+  ) => TermElm m (Term ts (GChr r xs)) (is:.Subword) where
+  type TermOf (Term ts (GChr r xs)) = TermOf ts :. r
+  termStream (ts :! GChr f xs) (io:.Outer) (is:.ij@(Subword(i:.j))) =
+    let dta = f xs (j-1)
+    in  dta `seq` S.map (\(zs :!: (zix:.kl) :!: zis :!: e) -> (zs :!: zix :!: (zis:.kl) :!: (e:.dta)))
+        . termStream ts io is
+        . S.map (\(zs :!: zix :!: (zis:.kl)) -> (zs :!: (zix:.kl) :!: zis))
+  {-# INLINE termStream #-}
 
 
 type instance ParserRange Z = Z
