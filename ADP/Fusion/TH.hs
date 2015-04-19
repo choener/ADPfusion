1885687af65b754abb3194b108a5e433fc59fc65
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-04-19 02:44:01 +0200

    renaming and explicit types for TH

--- ADP/Fusion/TH.hs
+++ ADP/Fusion/TH.hs
@@ -12,7 +12,7 @@
 --
 -- TODO Then we also need @***@ handling the case of vector-to-vector results.
 --
--- TODO note the comments in @buildChoice@
+-- TODO note the comments in @buildBacktrackingChoice@
 
 module ADP.Fusion.TH where
 
@@ -27,7 +27,7 @@ import qualified Data.Vector.Fusion.Stream.Monadic as SM
 --
 -- TODO make the resulting function INLINE
 --
--- TODO compare @ntTypes@ with the stream argument types of all @hs@ (via their
+-- TODO compare @synTypes@ with the stream argument types of all @hs@ (via their
 -- @hns@ names). If there is a mismatch, then either not all non-terminal types
 -- have a corresponding choice function or vice versa.
 
@@ -43,7 +43,7 @@ makeAlgebraProductH hns nm = do
         -- split @fs@ into functions applied to rule RHSs and choice functions (@hs@)
         let (fs,hs) = partition ((`notElem` hns) . sel1) fs'
         -- the result types of the @fs@ are the types of the non-terminal symbols
-        let ntTypes = nub . map getNtTypes $ fs
+        let synTypes = nub . map getRuleResultType $ fs
         fun <- funD (mkName "<**") [genClause dataConName fs' fs hs]
         return
           [ fun
@@ -51,11 +51,18 @@ makeAlgebraProductH hns nm = do
       _   -> fail "more than one data ctor"
     _          -> fail "unsupported data type"
 
+
 -- | Build the single clause of our function. We shall need the functions bound
 -- in the where part to create the joined functions we need to return.
 
+genClause
+  :: Name
+  -> [VarStrictType]
+  -> [VarStrictType]
+  -> [VarStrictType]
+  -> Q Clause
 genClause conName allFunNames evalFunNames choiceFunNames = do
-  let nonTermNames = nub . map getNtTypes $ evalFunNames
+  let nonTermNames = nub . map getRuleResultType $ evalFunNames
   -- bind the l'eft and r'ight variable of the two algebras we want to join,
   -- also create unique names for the function names we shall bind later.
   nameL <- newName "l"
@@ -70,70 +77,98 @@ genClause conName allFunNames evalFunNames choiceFunNames = do
   whereR <- valD (conP conName (map varP fnmsR)) (normalB $ varE nameR) []
   rce <- recConE conName
           $  zipWith3 (genChoiceFunction) (drop (length evalFunNames) fnmsL) (drop (length evalFunNames) fnmsR) choiceFunNames
-          ++ zipWith3 (genEvalFunction nonTermNames) fnmsL fnmsR evalFunNames
+          ++ zipWith3 (genAttributeFunction nonTermNames) fnmsL fnmsR evalFunNames
   -- build the function pairs
   -- to keep our sanity, lets print this stuff
   let cls = Clause [varL, varR] (NormalB rce) [whereL,whereR]
   return cls
 
+
+-- |
+
+genChoiceFunction
+  :: Name
+  -> Name
+  -> VarStrictType
+  -> Q (Name,Exp)
 genChoiceFunction hL hR (name,_,t) = do
-  exp <- buildChoice hL hR
+  exp <- buildBacktrackingChoice hL hR
   return (name,exp)
+
 
 -- |
 --
 -- TODO need fun names from @l@ and @r@
 
-genEvalFunction nts fL fR (name,_,t) = do
-  (lamPat,funL,funR) <-recBuildLamPat nts fL fR $ init $ getNames t -- @init@ since we don't want the result as a parameter
+genAttributeFunction
+  :: [Name]
+  -> Name
+  -> Name
+  -> VarStrictType
+  -> Q (Name,Exp)
+genAttributeFunction nts fL fR (name,_,t) = do
+  (lamPat,funL,funR) <-recBuildLamPat nts fL fR (init $ getRuleSynVarNames t) -- @init@ since we don't want the result as a parameter
   let exp = LamE lamPat $ TupE [funL,funR]
   return (name,exp)
 
 -- |
 
-recBuildLamPat :: [Name] -> Name -> Name -> [[Name]] -> Q ([Pat], Exp, Exp)
+recBuildLamPat :: [Name] -> Name -> Name -> [Name] -> Q ([Pat], Exp, Exp)
 recBuildLamPat nts fL' fR' ts = do
-  ps <- sequence [ if length t==1 && head t `elem` nts then tupP [newName "x" >>= varP, newName "ys" >>= varP] else (newName "t" >>= varP) | t<-ts]
+  ps <- sequence [ if t `elem` nts then tupP [newName "x" >>= varP, newName "ys" >>= varP] else (newName "t" >>= varP) | t<-ts]
   let buildLfun f (TupP [VarP v,_]) = appE f (varE v)
       buildLfun f (VarP v         ) = appE f (varE v)
   lfun <- foldl buildLfun (varE fL') ps
   rfun <- buildRns (VarE fR') [] ps
   return (ps, lfun, rfun)
 
+
+-- |
+
+buildRns
+  :: Exp
+  -> [Name]
+  -> [Pat]
+  -> ExpQ
 buildRns f xs []                     = appE ([| return . SM.singleton |]) (foldl (\g z -> appE g (varE z)) (return f) xs)
 buildRns f xs (VarP v          : ys) = buildRns f (xs++[v]) ys
 buildRns f xs (TupP [_,VarP v] : ys) = do w  <- newName "w"
                                           [| $(varE v) >>= return . SM.concatMapM $(lamE [varP w] (buildRns f (xs++[w]) ys)) |]
 
--- | Build up the choice function.
+-- | Build up the backtracking choice function. This choice function will
+-- backtrack based on the first result, then return only the second.
 --
 -- TODO it should be (only?) this function we will need to modify to build all
 -- algebra products.
 
-buildChoice :: Name -> Name -> Q Exp
-buildChoice hL' hR' = do
+buildBacktrackingChoice :: Name -> Name -> Q Exp
+buildBacktrackingChoice hL' hR' = do
   [| \xs -> do hfs <- $(varE hL') $ SM.map fst xs
                let phfs = SM.concatMapM snd . SM.filter ((hfs==) . fst) $ xs
                $(varE hR') phfs |]
 
+
 -- | Gets the names used in the evaluation function.
-
-getNames :: Type -> [[Name]]
-getNames t' = go t' where
-  go t
-    | VarT x <- t = [[x]]
-    | AppT (AppT ArrowT (VarT x  )) y <- t = [x] : go y
-    | AppT (AppT ArrowT (AppT _ _)) y <- t = []  : go y  -- later on, grab all terminal names in a multi-dim case
-    | AppT (AppT ArrowT (TupleT 0)) y <- t = []  : go y   -- this case captures things like @nil :: () -> x@ for rules like @nil <<< Epsilon@.
-    | otherwise            = error $ "getNames error: " ++ show t ++ "    in:    " ++ show t'
-
+--
+-- @
 -- AppT (AppT ArrowT (AppT (AppT (ConT Data.Array.Repa.Index.:.) (AppT (AppT (ConT Data.Array.Repa.Index.:.) (ConT Data.Array.Repa.Index.Z)) (VarT c_1627675270))) (VarT c_1627675270))) (VarT x_1627675265)
+-- @
 
--- | Get us the 'Name' of a non-terminal type. Theses are simply the last
--- @VarT@s occuring in a function.
+getRuleSynVarNames :: Type -> [Name]
+getRuleSynVarNames t' = go t' where
+  go t
+    | VarT x <- t = [x]
+    | AppT (AppT ArrowT (VarT x  )) y <- t = x : go y   -- this is a syntactic variable, return the name that the incoming data is bound to
+    | AppT (AppT ArrowT (AppT _ _)) y <- t =     go y   -- this is a terminal
+    | AppT (AppT ArrowT (TupleT 0)) y <- t =     go y   -- this case captures things like @nil :: () -> x@ for rules like @nil <<< Epsilon@.
+    | otherwise            = error $ "getRuleSynVarNames error: " ++ show t ++ "    in:    " ++ show t'
 
-getNtTypes :: VarStrictType -> Name
-getNtTypes vst = go $ sel3 vst where
+
+-- | The last @Name@ of a rule is the name of the syntactic type of the
+-- result.
+
+getRuleResultType :: VarStrictType -> Name
+getRuleResultType vst = go $ sel3 vst where
   go t
     | AppT _ (VarT x) <- t = x
     | AppT _ x        <- t = go x
