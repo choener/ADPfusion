dbcd8a303d73d44eecf9fb592fd6600aeb3ef5e2
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-06-16 02:02:33 +0200

    forward Strng terminal mkStream implemented

--- ADP/Fusion/Term/Strng/Subword.hs
+++ ADP/Fusion/Term/Strng/Subword.hs
@@ -3,7 +3,10 @@ module ADP.Fusion.Term.Strng.Subword where
 
 
 import           Data.Strict.Tuple
+import           Data.Vector.Fusion.Stream.Size
+import           Data.Vector.Fusion.Util (delay_inline)
 import           Debug.Trace
+import           Prelude hiding (map)
 import qualified Data.Vector.Fusion.Stream.Monadic as S
 import qualified Data.Vector.Generic as VG
 
@@ -14,10 +17,28 @@ import           ADP.Fusion.Term.Strng.Type
 
 
 
+-- | TODO If we use (IVariable mx) we might be able to request @exactly@
+-- the range we need!
+
 instance
   ( Monad m
   , Element ls Subword
   , MkStream m ls Subword
   ) => MkStream m (ls :!: Strng v x) Subword where
-
+  mkStream (ls :!: Strng slice mn mx v) (IStatic ()) hh (Subword (i:.j))
+    = S.filter (\s -> let Subword (k:.l) = getIdx s in l-k <= mx)
+    . S.map (\s -> let (Subword (_:.l)) = getIdx s
+                   in  ElmStrng (slice l (j-l) v) (subword l j) (subword 0 0) s)
+    $ mkStream ls (IVariable ()) hh (delay_inline Subword (i:.j - mn))
+  mkStream (ls :!: Strng slice mn mx v) (IVariable ()) hh (Subword (i:.j))
+    = S.flatten mk step Unknown $ mkStream ls (IVariable ()) hh (delay_inline Subword (i:.j - mn))
+    where mk s = let Subword (_:.l) = getIdx s in return (s :. j - l - mn)
+          step (s:.z) | z >= 0 = do let Subword (_:.k) = getIdx s
+                                        l              = j - z
+                                        kl             = subword k l
+                                    return $ S.Yield (ElmStrng (slice k (l-k) v) kl (subword 0 0) s) (s:.z-1)
+                      | otherwise = return $ S.Done
+          {-# Inline [0] mk   #-}
+          {-# Inline [0] step #-}
+  {-# Inline mkStream #-}
 
