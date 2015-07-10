5101c916529df46b7c71ba75e3a79ffc97ce2acb
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-07-10 18:22:44 +0200

    fixing up pseudoknot c example
    part 1: nussinov

--- C/pseudoknot.c
+++ C/pseudoknot.c
@@ -1,18 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-#define MAX(a,b) ( ((a) > (b)) ? (a) : (b) )
+#define MAX(a,b)      ( ((a) > (b)) ? (a) : (b) )
+#define I2(n,i,j)     ((i)*n+(j))
+#define I4(n,i,j,k,l) ((i)*n^3 + (j)*n^2 + (k)*n + (l))
 
 int pseudoknot (int, char *);
 int pairs (char, char);
 
 int main () {
-  char p[10000];
+  char *p = calloc (10000, sizeof(char));
   int n;
   int e;
-  while (1==scanf ("%9999s", &p)) { // only GNU C
+  int i;
+  while (1==scanf ("%9999s", p)) { // only GNU C
     n = strlen(p);
+    for (i=0;i<n;i++) {
+      p[i] = toupper(p[i]);
+    }
     e = pseudoknot (n, p);
     printf ("%s\n%d\n", p, e);
   };
@@ -32,25 +39,35 @@ int pairs (char l,char r) {
 int pseudoknot (int n, char *inp) {
   int i, j, k, l, a, b, c;
   //int at;
-  int cur;
-  int new;
+  int cur, new, newL, newR;
   int *t = calloc (n*n    , sizeof(int));
   int *u = calloc (n*n*n*n, sizeof(int));
   int *v = calloc (n*n*n*n, sizeof(int));
+  // init
+  for (i=0; i<n; i++) for (j=0; j<n; j++) {
+    t[I2(n,i,j)] = -999999;
+    for (k=0; k<n; k++) for (l=0; l<n; l++) {
+      u[I4(n,i,j,k,l)] = -999999;
+      v[I4(n,i,j,k,l)] = -999999;
+    }
+  }
 
   for (i = n-1; i>=0; i--) for (j=i; j<n; j++) {
     // fill t table
     cur = -999999;
     if (j>0) {  // T -> T c
-      new = t[i*n+j-1];
+      new = t[I2(n,i,j)];
       cur = MAX(cur,new);
     }; 
-    for (a=i+1; a<j-1; a++) {
+    for (a=i; a<=j; a++) {
       new = -999999;
+      newL = a<=i     ? 0 : t[I2(n,i,a-1)];
+      newR = a+1>=j-1 ? 0 : t[I2(n,a+1,j-1)];
       if (pairs (inp[a],inp[j])) {
-        new = t[i*n+a-1] + t[(a+1)*n+j-1] + 1;
+        new = newL + newR + 1;
+        cur = MAX(cur,new);
+//        printf ("P %3d %c %3d %c -- %4d + %4d + 1\n",a, inp[a],j, inp[j], newL, newR);
       }
-      cur = MAX(cur,new);
     }; // n^3 loop
     for (a=i; a<j; a++) for (b=a; b<j; b++) for (c=b; c<j; c++) {
       // ~~ i ~~ a ~~ b ~~ c ~~ j ~~
@@ -58,58 +75,40 @@ int pseudoknot (int n, char *inp) {
       cur = MAX(cur,new);
     }; // n^5 pseudoknot loop
     t[i*n+j] = cur;
-    for (k=i; k<=j; k++) for (l=k; l<=j; l++) {
-      // u
-      cur = -999999;
-      // nil, nil
-      if (i==k && l==j)
-        cur = 0;
-      // loop over inner part
-      for (a=i+1; a<k; a++) for (b=l; b<j; b++) {
-        if (pairs(inp[a], inp[j])) {
-          new = t[i*n+a-1] + u[(a+1)*n^3+k*n^2+b*n+j] + t[l*n+b] + 1;
-          cur = MAX(cur,new);
-        };
-      };
-      u[i*n^3+k*n^2+l*n+j] = cur;
+    printf ("  %3d %c %3d %c %4d\n",i,inp[i],j,inp[j], t[I2(n,i,j)]);
+    //for (k=i; k<=j; k++) for (l=k; l<=j; l++) {
+    //  // u
+    //  cur = -999999;
+    //  // nil, nil
+    //  if (i==k && l==j)
+    //    cur = 0;
+    //  // loop over inner part
+    //  for (a=i+1; a<k; a++) for (b=l; b<j; b++) {
+    //    if (pairs(inp[a], inp[j])) {
+    //      new = t[i*n+a-1] + u[(a+1)*n^3+k*n^2+b*n+j] + t[l*n+b] + 1;
+    //      cur = MAX(cur,new);
+    //    };
+    //  };
+    //  u[i*n^3+k*n^2+l*n+j] = cur;
 
-      // v
-      cur = -999999;
-      // nil, nil
-      if (i==k && l==j)
-        cur = 0;
-      // loop over inner part
-      for (a=i+1; a<k; a++) for (b=l; b<j; b++) {
-        if (pairs(inp[a], inp[j])) {
-          new = t[i*n+a-1] + v[(a+1)*n^3+k*n^2+b*n+j] + t[l*n+b] + 1;
-          cur = MAX(cur,new);
-        };
-      };
-      v[i*n^3+k*n^2+l*n+j] = cur;
-    } // n^6 loop for u,v
-      // fill u, v tables
+    //  // v
+    //  cur = -999999;
+    //  // nil, nil
+    //  if (i==k && l==j)
+    //    cur = 0;
+    //  // loop over inner part
+    //  for (a=i+1; a<k; a++) for (b=l; b<j; b++) {
+    //    if (pairs(inp[a], inp[j])) {
+    //      new = t[i*n+a-1] + v[(a+1)*n^3+k*n^2+b*n+j] + t[l*n+b] + 1;
+    //      cur = MAX(cur,new);
+    //    };
+    //  };
+    //  v[i*n^3+k*n^2+l*n+j] = cur;
+    //} // n^6 loop for u,v
   } // the n^2 loop
 
-//  for (i = 0 ; i<m; i++) for (j=0; j<n; j++) {
-//    cur = 0;
-//    if (i==0 && j==0) { // nil_nil
-//      cur = p[i] == q[j];
-//    }
-//    if (i>0) { // step_loop
-//      new = mat[(i-1) * m + j] -1;
-//      if (cur < new) cur = new;
-//    }
-//    if (j>0) { // loop_step
-//      new = mat[i * m + (j-1)] -1;
-//      if (cur < new) cur = new;
-//    }
-//    if (i>0 && j>0) { // loop_loop
-//      new = mat[(i-1) * m + (j-1)];
-//      new += ((p[i] == q[j]) ? 1 : (-1));
-//      if (cur < new) cur = new;
-//    }
-//    mat[i*m+j] = cur;
-//  };
+  cur = t[I2(n,0,n-1)];
+
   free (t);
   free (u);
   free (v);
