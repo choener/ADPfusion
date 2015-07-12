ce3023a5463d3986528038a9093b39ca43b21222
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-07-12 15:55:14 +0200

    rather complex benchmark runner

--- C/bench-run.sh
+++ C/bench-run.sh
@@ -4,14 +4,16 @@
 #   stdin: lines
 #   $1: program to use
 
+typeset -A k
 typeset -A t
 typeset -A m
 
 while read i
 do
+  k[${#i}]="${#i}"
 #  echo $i
-  t[${#i}]+=" ${#i}"
-  m[${#i}]+=" ${#i}"
+#  t[${#i}]+=" ${#i}"
+#  m[${#i}]+=" ${#i}"
 #  echo $i | /usr/bin/time -f "%e %M" $1
   var1=`echo $i | /usr/bin/time -f "%e %M" $1` 2>&1 | read var2
 #  echo "O $var1"
@@ -21,6 +23,27 @@ do
   m[${#i}]+=" $a[2]"
 done
 
-print $t
-print $m
+#print $k
+#print $t
+#print $m
+
+echo "time"
+
+for i in "${(@k)k}"; do
+  # echo "$i -> $t[$i]"
+  n=("${(@s/ /)t[$i]}")
+  n[1]=0
+  (( sum=${(j:+:)n} ))
+  echo $i $(( $sum / $(( ${#n} - 1 )) ))
+done | sort -n
+
+echo "mem"
+
+for i in "${(@k)k}"; do
+  # echo "$i -> $t[$i]"
+  n=("${(@s/ /)m[$i]}")
+  n[1]=0
+  (( sum=${(j:+:)n} ))
+  echo $i $(( $sum / $(( ${#n} - 1 )) ))
+done | sort -n
 
