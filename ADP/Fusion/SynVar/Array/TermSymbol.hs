95b905ac7e05b70f72b7013ce9df20c9efa016c3
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-10-09 16:51:15 +0200

    new impl for mcfg synvar / term

--- ADP/Fusion/SynVar/Array/TermSymbol.hs
+++ ADP/Fusion/SynVar/Array/TermSymbol.hs
@@ -1,8 +1,13 @@
 
 -- | TODO migrate instances to correct modules
+--
+-- TODO need to find out if we can reduce the total number of instances
+-- required here. Probably not trivial since there are, in principle, @n*m@
+-- instances that we need to handle.
 
 module ADP.Fusion.SynVar.Array.TermSymbol where
 
+import Data.Proxy
 import Data.Strict.Tuple hiding (snd)
 import Data.Vector.Fusion.Util (delay_inline)
 import Data.Vector.Fusion.Stream.Monadic hiding (flatten)
@@ -17,54 +22,112 @@ import ADP.Fusion.SynVar.Backtrack
 
 
 
+-- |
+--
+-- TODO need to handle @minSize@ conditions!
+
+instance
+  ( TstCtx1 m ts a is (Subword I)
+  , PrimArrayOps arr (Subword I) x
+  ) => TermStream m (TermSymbol ts (ITbl m arr (Subword I) x)) a (is:.Subword I) where
+  --
+  termStream (ts:|ITbl _ _ _ t _) (cs:.IStatic ()) (us:.u) (is:.Subword (i:.j))
+    = map (\(TState s a b ii oo ee) ->
+              let Subword (_:.l) = getIndex a (Proxy :: Proxy (is:.Subword I))
+                  lj             = subword l j
+              in  TState s a b (ii:.lj) (oo:.subword 0 0) (ee:.t!lj) )
+    . termStream ts cs us is
+  --
+  termStream (ts:|ITbl _ _ _ t _) (cs:.IVariable ()) (us:.u) (is:.Subword (i:.j))
+    = flatten mk step . termStream ts cs us is
+    where mk tstate@(TState s a b ii oo ee) =
+              let Subword (_:.l) = getIndex a (Proxy :: Proxy (is:.Subword I))
+              in  return (tstate, l, j - l)
+          step (tstate@(TState s a b ii oo ee), k, z)
+            | z >= 0 = do let l  = j - z
+                              kl = subword k l
+                          return $ Yield (TState s a b (ii:.kl) (oo:.subword 0 0) (ee:.t!kl)) (tstate, k, z-1)
+            | otherwise = return $ Done
+          {-# Inline [0] mk   #-}
+          {-# Inline [0] step #-}
+  {-# Inline termStream #-}
+
+-- |
+--
+-- TODO can we combine the @ITbl@ and @BtITbl@ code again?
+
+instance
+  ( TstCtx1 mB ts a is (Subword I)
+  , PrimArrayOps arr (Subword I) x
+  ) => TermStream mB (TermSymbol ts (Backtrack (ITbl mF arr (Subword I) x) mF mB r)) a (is:.Subword I) where
+  termStream (ts:|BtITbl c t bt) (cs:.IStatic ()) (us:.u) (is:.Subword (i:.j))
+    = mapM (\(TState s a b ii oo ee) ->
+                let Subword (_:.l) = getIndex a (Proxy :: Proxy (is:.Subword I))
+                    lj             = subword l j
+                in  bt u lj >>= \ ~bb -> return $ TState s a b (ii:.lj) (oo:.subword 0 0) (ee:.(t!lj,bb)) )
+    . termStream ts cs us is
+  termStream (ts:|BtITbl c t bt) (cs:.IVariable ()) (us:.u) (is:.Subword (i:.j))
+    = flatten mk step . termStream ts cs us is
+    where mk tstate@(TState s a b ii oo ee) =
+              let Subword (_:.l) = getIndex a (Proxy :: Proxy (is:.Subword I))
+              in  return (tstate, l, j - l)
+          step (tstate@(TState s a b ii oo ee), k, z)
+            | k >= 0 = do let l  = j - z
+                              kl = subword k l
+                          bt u kl >>= \ ~bb -> return $ Yield (TState s a b (ii:.kl) (oo:.subword 0 0) (ee:.(t!kl,bb))) (tstate, k, z-1)
+            | otherwise = return $ Done
+          {-# Inline [0] mk   #-}
+          {-# Inline [0] step #-}
+  {-# Inline termStream #-}
+
 -- | TODO need to deal with @minSize@
 
-instance
-  ( Monad m
-  , TerminalStream m a is
-  , PrimArrayOps arr (Subword I) x
-  , Show x
-  ) => TerminalStream m (TermSymbol a (ITbl m arr (Subword I) x)) (is:.Subword I) where
-  terminalStream (a :| ITbl _ _ c t _) (sv:.IStatic _) (is:.ix@(Subword (i:.j)))
-    = map (\ (S6 s (zi:.(Subword (a:.l))) (zo:._) is os e) ->
-              let lj = subword l j
-              in  {- traceShow (i,a,' ',l,j,t!lj) $ -} S6 s zi zo (is:.lj) (os:.subword 0 0) (e:.(t!lj)) )
-    . iPackTerminalStream a sv (is:.ix)
-  terminalStream (a :| ITbl _ _ c t _) (sv:.IVariable _) (is:.ix@(Subword (i:.j)))
-    = flatten mk step . iPackTerminalStream a sv (is:.ix)
-    where mk (S6 s (zi:.(Subword (_:.l))) (zo:._) is os e) = return (S6 s zi zo is os e :. l :. j - l) -- TODO minsize c !
-          step (s6:.k:.z) | z >= 0 = do let S6 s zi zo is os e = s6
-                                            l                  = j - z
-                                            kl                 = subword k l
-                                        return $ Yield (S6 s zi zo (is:.kl) (os:.subword 0 0) (e:.(t!kl))) (s6 :. k :. z-1)
-                          | otherwise = return $ Done
-          {-# Inline [0] mk   #-}
-          {-# Inline [0] step #-}
-  {-# Inline terminalStream #-}
+--instance
+--  ( Monad m
+--  , TerminalStream m a is
+--  , PrimArrayOps arr (Subword I) x
+--  , Show x
+--  ) => TerminalStream m (TermSymbol a (ITbl m arr (Subword I) x)) (is:.Subword I) where
+--  terminalStream (a :| ITbl _ _ c t _) (sv:.IStatic _) (is:.ix@(Subword (i:.j)))
+--    = map (\ (S6 s (zi:.(Subword (a:.l))) (zo:._) is os e) ->
+--              let lj = subword l j
+--              in  {- traceShow (i,a,' ',l,j,t!lj) $ -} S6 s zi zo (is:.lj) (os:.subword 0 0) (e:.(t!lj)) )
+--    . iPackTerminalStream a sv (is:.ix)
+--  terminalStream (a :| ITbl _ _ c t _) (sv:.IVariable _) (is:.ix@(Subword (i:.j)))
+--    = flatten mk step . iPackTerminalStream a sv (is:.ix)
+--    where mk (S6 s (zi:.(Subword (_:.l))) (zo:._) is os e) = return (S6 s zi zo is os e :. l :. j - l) -- TODO minsize c !
+--          step (s6:.k:.z) | z >= 0 = do let S6 s zi zo is os e = s6
+--                                            l                  = j - z
+--                                            kl                 = subword k l
+--                                        return $ Yield (S6 s zi zo (is:.kl) (os:.subword 0 0) (e:.(t!kl))) (s6 :. k :. z-1)
+--                          | otherwise = return $ Done
+--          {-# Inline [0] mk   #-}
+--          {-# Inline [0] step #-}
+--  {-# Inline terminalStream #-}
 
-instance
-  ( Monad mB
-  , TerminalStream mB a is
-  , PrimArrayOps arr (Subword I) x
-  ) => TerminalStream mB (TermSymbol a (Backtrack (ITbl mF arr (Subword I) x) mF mB r)) (is:.Subword I) where
-  terminalStream (a :| BtITbl c t bt) (sv:.IStatic _) (is:.ix@(Subword (i:.j)))
-    = mapM (\ (S6 s (zi:.(Subword (_:.l))) (zo:._) is os e) ->
-              let lj = subword l j
-                  hh = snd $ bounds t
-              in  bt hh lj >>= \ ~bb -> return $ S6 s zi zo (is:.lj) (os:.subword 0 0) (e:.(t!lj, bb)) )
-    . iPackTerminalStream a sv (is:.ix)
-  terminalStream (a :| BtITbl c t bt) (sv:.IVariable _) (is:.ix@(Subword (i:.j)))
-    = flatten mk step . iPackTerminalStream a sv (is:.ix)
-    where mk (S6 s (zi:.(Subword (_:.l))) (zo:._) is os e) = return (S6 s zi zo is os e :. l :. j - l) -- TODO minsize c !
-          step (s6:.k:.z) | z >= 0 = do let S6 s zi zo is os e = s6
-                                            l                  = j - z
-                                            kl                 = subword k l
-                                            hh                 = snd $ bounds t
-                                        bt hh kl >>= \ ~bb -> return $ Yield (S6 s zi zo (is:.kl) (os:.subword 0 0) (e:.(t!kl,bb))) (s6 :. k :. z-1)
-                          | otherwise = return $ Done
-          {-# Inline [0] mk   #-}
-          {-# Inline [0] step #-}
-  {-# Inline terminalStream #-}
+--instance
+--  ( Monad mB
+--  , TerminalStream mB a is
+--  , PrimArrayOps arr (Subword I) x
+--  ) => TerminalStream mB (TermSymbol a (Backtrack (ITbl mF arr (Subword I) x) mF mB r)) (is:.Subword I) where
+--  terminalStream (a :| BtITbl c t bt) (sv:.IStatic _) (is:.ix@(Subword (i:.j)))
+--    = mapM (\ (S6 s (zi:.(Subword (_:.l))) (zo:._) is os e) ->
+--              let lj = subword l j
+--                  hh = snd $ bounds t
+--              in  bt hh lj >>= \ ~bb -> return $ S6 s zi zo (is:.lj) (os:.subword 0 0) (e:.(t!lj, bb)) )
+--    . iPackTerminalStream a sv (is:.ix)
+--  terminalStream (a :| BtITbl c t bt) (sv:.IVariable _) (is:.ix@(Subword (i:.j)))
+--    = flatten mk step . iPackTerminalStream a sv (is:.ix)
+--    where mk (S6 s (zi:.(Subword (_:.l))) (zo:._) is os e) = return (S6 s zi zo is os e :. l :. j - l) -- TODO minsize c !
+--          step (s6:.k:.z) | z >= 0 = do let S6 s zi zo is os e = s6
+--                                            l                  = j - z
+--                                            kl                 = subword k l
+--                                            hh                 = snd $ bounds t
+--                                        bt hh kl >>= \ ~bb -> return $ Yield (S6 s zi zo (is:.kl) (os:.subword 0 0) (e:.(t!kl,bb))) (s6 :. k :. z-1)
+--                          | otherwise = return $ Done
+--          {-# Inline [0] mk   #-}
+--          {-# Inline [0] step #-}
+--  {-# Inline terminalStream #-}
 
 
 instance TermStaticVar (ITbl m arr (Subword I) x) (Subword I) where
