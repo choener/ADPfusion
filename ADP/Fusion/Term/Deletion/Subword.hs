a64b36b11a5fd3125492d7ec19ee1c904db21782
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-10-17 21:35:22 +0200

    deletion code for subword - outside

--- ADP/Fusion/Term/Deletion/Subword.hs
+++ ADP/Fusion/Term/Deletion/Subword.hs
@@ -40,10 +40,48 @@ instance
 instance
   ( TstCtx1 m ts a is (Subword O)
   ) => TermStream m (TermSymbol ts Deletion) a (is:.Subword O) where
+  -- X_ij  -> Y_ik  Z_kj  d_jj        0   i Y k Z j-j   N
+  -- Y^_ik -> X^_ij Z_kj  d_jj        0 x i   k Z j-j x N
+  -- Z^_kj -> Y_ik  X^_ij d_jj        0 x i Y k   j-j x N
+  termStream (ts:|Deletion) (cs:.OStatic (di:.dj)) (us:.u) (is:.Subword (i:.j))
+    = S.map (\(TState s a b ii oo ee) ->
+                let Subword (_:.k) = getIndex a (Proxy :: Proxy (is:.Subword O))
+                    o              = getIndex b (Proxy :: Proxy (is:.Subword O))
+                in  TState s a b (ii:.subword k k) (oo:.o) (ee:.()) )
+    . termStream ts cs us is
+  --
+  termStream (ts:|Deletion) (cs:.ORightOf (di:.dj)) (us:.u) (is:.Subword (i:.j))
+    = S.map (\(TState s a b ii oo ee) ->
+                let Subword (_:.k) = getIndex a (Proxy :: Proxy (is:.Subword O))
+                    o              = getIndex b (Proxy :: Proxy (is:.Subword O))
+                    l              = k - dj -- TODO needed ?
+                in  TState s a b (ii:.subword k k) (oo:.o) (ee:.()) )
+    . termStream ts cs us is
+  --
+  termStream (ts:|Deletion) (cs:.OFirstLeft (di:.dj)) (us:.u) (is:.Subword (i:.j))
+    = S.map (\(TState s a b ii oo ee) ->
+                let Subword (_:.k) = getIndex a (Proxy :: Proxy (is:.Subword O))
+                    o              = getIndex b (Proxy :: Proxy (is:.Subword O))
+                in  TState s a b (ii:.subword k k) (oo:.o) (ee:.()) )
+    . termStream ts cs us is
+  --
+  termStream (ts:|Deletion) (cs:.OLeftOf (di:.dj)) (us:.u) (is:.Subword (i:.j))
+    = S.map (\(TState s a b ii oo ee) ->
+                let Subword (_:.k) = getIndex a (Proxy :: Proxy (is:.Subword O))
+                    o              = getIndex b (Proxy :: Proxy (is:.Subword O))
+                in  TState s a b (ii:.subword k k) (oo:.o) (ee:.()) )
+    . termStream ts cs us is
+  {-# Inline termStream #-}
 
 
 
 instance TermStaticVar Deletion (Subword I) where
+  termStaticVar _ sv _ = sv
+  termStreamIndex _ _ ij = ij
+  {-# Inline [0] termStaticVar   #-}
+  {-# Inline [0] termStreamIndex #-}
+
+instance TermStaticVar Deletion (Subword O) where
   termStaticVar _ sv _ = sv
   termStreamIndex _ _ ij = ij
   {-# Inline [0] termStaticVar   #-}
