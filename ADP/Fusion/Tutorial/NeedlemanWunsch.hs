cdecb7574a077afdaa96fbaa50684d94d7a474d1
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-08-18 21:33:45 +0200

    tutorial for NW to be written in here

---
+++ ADP/Fusion/Tutorial/NeedlemanWunsch.hs
@@ -0,0 +1,20 @@
+
+{-# Options_GHC -fno-warn-unused-imports #-}
+
+{-|
+
+    zzz
+
+ -}
+
+module ADP.Fusion.Tutorial.NeedlemanWunsch
+  (
+  -- * introduction
+  -- $introduction
+  ) where
+
+import ADP.Fusion.Point
+
+{- $introduction
+
+-}
