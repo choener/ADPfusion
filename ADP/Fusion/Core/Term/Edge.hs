d52c83fd13240006380919e4c74c8c4ddd007ea7
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-02-17 18:01:03 +0100

    moved stuff into Edge

--- ADP/Fusion/Core/Term/Edge.hs
+++ ADP/Fusion/Core/Term/Edge.hs
@@ -2,6 +2,7 @@
 module ADP.Fusion.Core.Term.Edge where
 
 import Data.Strict.Tuple
+import Data.Proxy
 
 import Data.PrimitiveArray
 
@@ -36,4 +37,31 @@ instance
 deriving instance (Show i, Show (RunningIndex i), Show (Elm ls i)) => Show (Elm (ls :!: Edge) i)
 
 type instance TermArg Edge = (From:.To)
+
+-- | 'edgeFromTo' creates a @(From:.To)@ structure for edges. How this is
+-- filled depends on the @Proxy@. Possible are @Proxy First@ and @Proxy Last@.
+-- @First@ denotes that @To@ is the first node to be visited. I.e. @First(From)
+-- → Set(To)@. @Last@ on the other hand is @Set(From) → Last(To)@.
+
+class EdgeFromTo k where
+  edgeFromTo ∷ Proxy k → SetBoundary → NewBoundary → (From:.To)
+
+newtype SetBoundary = SetBoundary Int
+
+newtype NewBoundary = NewBoundary Int
+
+---- | In case our sets have a @First@ boundary, then we always point from
+---- the boundary "into" the set. Hence @SetNode == To@ and @NewNode ==
+---- From@.
+--
+--instance EdgeFromTo First where
+--  edgeFromTo Proxy (SetNode to) (NewNode from) = From from :. To to
+--  {-# Inline edgeFromTo #-}
+
+-- | And if the set has a @Last@ boundary, then we point from somewhere in
+-- the set @To@ the @NewNode@, which is @Last@.
+
+instance EdgeFromTo Last where
+  edgeFromTo Proxy (SetBoundary from) (NewBoundary to) = From from :. To to
+  {-# Inline edgeFromTo #-}
 
