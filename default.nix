09ee7d65633e515e0ebd7d9c976ab2e2a38f76e3
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-05-03 18:40:12 +0200

    playing with nix expressions

--- default.nix
+++ default.nix
@@ -1,6 +1,8 @@
 with import <nixpkgs> {};
 haskellPackages.extend (haskell.lib.packageSourceOverrides {
-  # DPutils = ../Lib-DPutils;
-  # OrderedBits = ../Lib-OrderedBits;
-  # PrimitiveArray = ../Lib-PrimitiveArray;
+  DPutils = ../Lib-DPutils;
+  OrderedBits = ../Lib-OrderedBits;
+  PrimitiveArray = ../Lib-PrimitiveArray;
+#  doctest = "0.12.0";
 })
+
--- shell.nix
+++ shell.nix
@@ -1,4 +1,5 @@
 (import ./.).shellFor {
-  packages = p: [ ];
-  # p.DPutils p.OrderedBits p.PrimitiveArray p.ADPfusion ];
+  packages = p: [ p.DPutils p.OrderedBits p.PrimitiveArray p.ADPfusion ];
+  withHoogle = true;
 }
+
