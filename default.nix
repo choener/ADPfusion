3f37032384b20d687ba4572be51f5e5a33c2463c
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-05-25 16:32:26 +0200

    seems to build with yet another way to do NIX

--- default.nix
+++ default.nix
@@ -1,8 +1,19 @@
-with import <nixpkgs> {};
-haskellPackages.extend (haskell.lib.packageSourceOverrides {
-  DPutils = ../Lib-DPutils;
-  OrderedBits = ../Lib-OrderedBits;
-  PrimitiveArray = ../Lib-PrimitiveArray;
-#  doctest = "0.12.0";
-})
+with (import <nixpkgs> {});
+with haskell.lib;
 
+rec {
+  hsPkgs = haskellPackages.extend (packageSourceOverrides {
+    DPutils = ../Lib-DPutils;
+    OrderedBits = ../Lib-OrderedBits;
+    PrimitiveArray = ../Lib-PrimitiveArray;
+    ADPfusion = ./.;
+  });
+  hsShell = with hsPkgs; shellFor {
+    packages = p: [ p.ADPfusion ];
+    withHoogle = true;
+    buildInputs = [
+      cabal-install ghc
+      OrderedBits DPutils PrimitiveArray
+    ];
+  };
+}
--- shell.nix
+++
@@ -1,5 +0,0 @@
-(import ./.).shellFor {
-  packages = p: [ p.DPutils p.OrderedBits p.PrimitiveArray p.ADPfusion ];
-  withHoogle = true;
-}
-
