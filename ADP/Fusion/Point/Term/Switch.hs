9925105ca6d37c3c226ff43185e56edfe382034a
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-10-03 16:52:41 +0200

    added a possible important bug (trac on datatotag) notice

--- ADP/Fusion/Point/Term/Switch.hs
+++ ADP/Fusion/Point/Term/Switch.hs
@@ -62,6 +62,7 @@ instance
 
 instance TermStaticVar (IStatic d) Switch (PointL I) where
   termStreamIndex Proxy (Switch s) (PointL j) = PointL $ j
+  -- TODO is trac #15696 a problem here?
   termStaticCheck Proxy (Switch s) (PointL j) grd = dataToTag# s `andI#` grd -- case s of {Enabled → grd; Disabled → 0# }
   {-# Inline [0] termStreamIndex #-}
   {-# Inline [0] termStaticCheck #-}
