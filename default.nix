f5a5107a7e2a70811d8de41cc0a9353c9acf3bd1
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-04 00:15:11 +0100

    move to envhs

--- default.nix
+++ default.nix
@@ -1,2 +1,28 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, base, bits, containers, deepseq, DPutils, fmlist
+, ghc-prim, mmorph, mtl, OrderedBits, primitive, PrimitiveArray
+, QuickCheck, singletons, stdenv, strict, tasty, tasty-quickcheck
+, tasty-th, template-haskell, th-orphans, transformers, tuple
+, vector
+}:
+mkDerivation {
+  pname = "ADPfusion";
+  version = "0.6.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    base bits containers deepseq DPutils ghc-prim mmorph mtl
+    OrderedBits primitive PrimitiveArray QuickCheck singletons strict
+    template-haskell th-orphans transformers tuple vector
+  ];
+  testHaskellDepends = [
+    base bits OrderedBits PrimitiveArray QuickCheck strict tasty
+    tasty-quickcheck tasty-th vector
+  ];
+  benchmarkHaskellDepends = [
+    base fmlist PrimitiveArray template-haskell vector
+  ];
+  homepage = "https://github.com/choener/ADPfusion";
+  description = "Efficient, high-level dynamic programming";
+  license = stdenv.lib.licenses.bsd3;
+}
