9a2337f571c1c2f0d759f604310a838dd2861dc7
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2021-03-12 11:30:48 +0100

    updated flake file

--- flake.nix
+++ flake.nix
@@ -1,32 +1,69 @@
 {
   description = ''
-    Generic Haskell development flake
-    This flake pins "nixpkgs" but not much more.
-    Afterwards, the "real" nix shell with ghc 8.10.2 is called within a zsh shell.
+    WienRNA is a secondary structure folding testbed to test how well ADPfusion performs.
   '';
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    # NOTE Only update if we are sure that all packages can build with nixos>20.09.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
     flake-utils.url = "github:numtide/flake-utils";
+    ghcicabal = { url = "github:choener/ghcicabal"; inputs.nixpkgs.follows = "nixpkgs"; };
+    DPutils-src = {
+      url = "github:choener/DPutils";
+      flake = false;
+    };
+    OrderedBits-src = {
+      url = "github:choener/OrderedBits";
+      flake = false;
+    };
+    PrimitiveArray-src = {
+      url = "github:choener/PrimitiveArray";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShell = pkgs.mkShell {
-            "NIX_PATH" = "nixpkgs=${nixpkgs}";
-            shellHook = ''
-              nix-shell --run zsh
-              exit
-            '';
-          };
-          # NOTE unfortunately, this requires that all dependencies are known to the flake, which is
-          # not working out for this type of development, since I "share" in-progress repositories
-          # between multiple projects.
-          #devShell = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs = { self, nixpkgs, flake-utils, ghcicabal
+    , DPutils-src
+    , OrderedBits-src
+    , PrimitiveArray-src
+  }: let
+    over = final: prev: {
+      #haskellPackages = (prev.haskell.packages.ghc8102.override{ overrides= hself: hsuper: let
+      haskellPackages = (prev.haskellPackages.override{ overrides= hself: hsuper: let
+          checked   = a: hself.callHackageDirect a {};
+          unchecked = a: final.haskell.lib.dontCheck (checked a);
+          unb       = a: final.haskell.lib.dontCheck (final.haskell.lib.unmarkBroken a);
+        in {
+        };
+      }).extend ( hself: hsuper: {
+        ADPfusion = hself.callPackage ./. {};
+        #
+        DPutils = hself.callPackage DPutils-src {};
+        OrderedBits = hself.callPackage OrderedBits-src {};
+        PrimitiveArray = hself.callPackage PrimitiveArray-src {};
+      });
+    };
+  in
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; overlays = [ ghcicabal.overlay self.overlay ]; };
+      sharedBuildInputs = with pkgs; [ llvm ];
+    in {
+      # update dependencies via mr, develop the package, push changes, and update the flake
+      # dependencies if major changes were made or before releasing
+      devShell = pkgs.haskellPackages.shellFor {
+        packages = p: [
+          p.ADPfusion
+          #
+          p.DPutils
+          p.OrderedBits
+          p.PrimitiveArray
+        ];
+        withHoogle = true;
+        buildInputs = with pkgs; [
+          cabal-install
+          pkgs.ghcicabal # be explicit to get the final package
+        ] ++ sharedBuildInputs;
+      }; # devShell
+    }) // { overlay = over; };
 }
 
