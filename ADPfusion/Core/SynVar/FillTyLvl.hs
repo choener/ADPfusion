ebc9dc295bc9d0f22795de2e0aa59918bd382c59
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2022-07-04 17:49:36 +0200

    WIP heterogeniety problem

--- ADPfusion/Core/SynVar/FillTyLvl.hs
+++ ADPfusion/Core/SynVar/FillTyLvl.hs
@@ -8,25 +8,22 @@
 module ADPfusion.Core.SynVar.FillTyLvl where
 
 import           Control.DeepSeq
+import           Control.Monad
 import           Control.Monad.Primitive
 import           Control.Monad.ST
-import           Control.Monad
-import           Data.Proxy
---import           Data.Singletons.Prelude.Bool
---import           Data.Singletons.Prelude.Bool
---import           Data.Singletons.Prelude.List
 import           Data.Bool.Singletons
 import           Data.List.Singletons
+import           Data.Proxy
 import           Data.Type.Equality
 import           Data.Vector.Fusion.Util (Id(..))
 import           Debug.Trace (traceShow)
 import           GHC.Exts
 import           GHC.Generics
 import           GHC.TypeNats
+import qualified Data.Vector as V
 import qualified Data.Vector.Fusion.Stream.Monadic as SM
 import qualified Data.Vector.Generic as VG
 import qualified Data.Vector.Unboxed as VU
-import qualified Data.Vector as V
 import           System.CPUTime
 import           Text.Printf
 
@@ -38,17 +35,51 @@ import           ADPfusion.Core.SynVar.Array
 
 
 
+-- | This version of @fillTables@ requires an explicit 'LimitType', which allows us to handle
+-- heterogenous tables.
+--
+-- The 'BigOrderNats' type-level function extracts the number of stages.
+--
+-- TODO This is still kind-of a hack, since we need to be more explicit yet on how to fill tables
+-- that are shape-smaller than the given shape ...
+--
+-- TODO we should actually have a LimitType thing for each individual big order table, since those
+-- should be possible to be different.
+
+fillTablesDim
+  :: forall bigOrder s ts sh
+  .  (bigOrder ~ BigOrderNats ts, EachBigOrder bigOrder ts, CountNumberOfCells 0 ts)
+  => LimitType sh -> ts -> ST s (Mutated ts)
+--{{{
+{-# Inline fillTablesDim #-}
+fillTablesDim sh ts = do
+  !startTime <- unsafeIOToPrim getCPUTime
+  ps <- eachBigOrder (Proxy :: Proxy bigOrder) ts
+  !stopTime  <- unsafeIOToPrim getCPUTime
+  let deltaTime = max 1 $ stopTime - startTime
+  return $! Mutated
+    { mutatedTables = ts
+    , perfCounter   = PerfCounter
+        { picoSeconds   = deltaTime
+        , seconds       = 1e-12 * fromIntegral deltaTime
+        , numberOfCells = countNumberOfCells (Nothing :: Maybe (Proxy 0)) ts
+        }
+    , eachBigPerfCounter = ps
+    }
+--}}}
+
 -- |
+--
+-- TODO we should have a variant that receives an explicit object with boundary dimensions. This
+-- will help with heterogenous table designs for pseudoknots. Then 'fillTables' will just extract
+-- the shape of the first table and use 'fillTablesDim'. Thereby we have a unified system for
+-- handling this.
 
 fillTables
   :: forall bigOrder s ts
-  .  ( bigOrder ~ BigOrderNats ts
-     , EachBigOrder bigOrder ts
-     , CountNumberOfCells 0 ts
-  )
-  => ts
-  -- ^ The tables
-  -> ST s (Mutated ts)
+  .  (bigOrder ~ BigOrderNats ts, EachBigOrder bigOrder ts, CountNumberOfCells 0 ts)
+  => ts -> ST s (Mutated ts)
+--{{{
 {-# Inline fillTables #-}
 fillTables ts = do
   !startTime <- unsafeIOToPrim getCPUTime
@@ -60,13 +91,16 @@ fillTables ts = do
     , perfCounter   = PerfCounter
         { picoSeconds   = deltaTime
         , seconds       = 1e-12 * fromIntegral deltaTime
-        , numberOfCells = countNumberOfCells (Nothing ∷ Maybe (Proxy 0)) ts
+        , numberOfCells = countNumberOfCells (Nothing :: Maybe (Proxy 0)) ts
         }
     , eachBigPerfCounter = ps
     }
+--}}}
 
 -- | This type class instanciates to the specialized machinery for each
--- @BigOrder Natural@ number.
+-- @BigOrder Natural@ number. @BigOrder@'s separate "logical stages" of a dynamic programming
+-- algorithm, that are not interdependent, but are rather run one after another. Of course later
+-- stages may use data from earlier stages, but not vice versa.
 
 class EachBigOrder (boNats :: [Nat]) ts where
   eachBigOrder :: Proxy boNats -> ts -> ST s [PerfCounter]
@@ -94,7 +128,7 @@ instance
     let p = PerfCounter
               { picoSeconds   = deltaTime
               , seconds       = 1e-12 * fromIntegral deltaTime
-              , numberOfCells = countNumberOfCells (Just (Proxy ∷ Proxy n)) ts
+              , numberOfCells = countNumberOfCells (Just (Proxy :: Proxy n)) ts
               }
     return $ p:ps
 
@@ -104,9 +138,9 @@ instance
 -- or a sparse set of elements, for @streamUp/streamDown@ to work on. This is currently not
 -- happening.
 
-class ThisBigOrder (boNat ∷ Nat) (thisOrder ∷ Bool) ts where
-  thisBigOrder ∷ Proxy boNat → Proxy thisOrder → ts → ST s ()
-  getAllBounds ∷ Proxy boNat → Proxy thisOrder → ts → [()]
+class ThisBigOrder (boNat :: Nat) (thisOrder :: Bool) ts where
+  thisBigOrder :: Proxy boNat -> Proxy thisOrder -> ts -> ST s ()
+  getAllBounds :: Proxy boNat -> Proxy thisOrder -> ts -> [()]
 
 instance ThisBigOrder boNat anyOrder Z where
   {-# Inline thisBigOrder #-}
@@ -127,11 +161,11 @@ instance
   {-# Inline thisBigOrder #-}
   thisBigOrder Proxy Proxy tst@(_:.TW (ITbl _ arr) _) = do
     let to = upperBound arr
-    let allBounds = getAllBounds (Proxy ∷ Proxy boNat) (Proxy ∷ Proxy True) tst
+    let allBounds = getAllBounds (Proxy :: Proxy boNat) (Proxy :: Proxy True) tst
     -- TODO This should provide one of a multitude of different ways to stream indices: for dense
     -- matrices, use what is below, but for other types, we will need unions of indices.
     flip SM.mapM_ (streamUp zeroBound' to) $ \k ->
-      eachSmallOrder (Proxy ∷ Proxy boNat) (Proxy ∷ Proxy smallOrder) tst k
+      eachSmallOrder (Proxy :: Proxy boNat) (Proxy :: Proxy smallOrder) tst k
   {-# Inline getAllBounds #-}
   getAllBounds Proxy Proxy (ts:.t) = undefined
 
@@ -150,7 +184,7 @@ instance
     let ixs = V.map (fromLinearIndex $ PAS.sparseUpperBound arr) $ V.convert $ PAS.sparseIndices arr
     traceShow "WARNING: mergeIndices has not happened yet! All sparse tables need to have the union of indices!" $
       flip V.mapM_ ixs $ \k ->
-        eachSmallOrder (Proxy ∷ Proxy boNat) (Proxy ∷ Proxy smallOrder) tst k
+        eachSmallOrder (Proxy :: Proxy boNat) (Proxy :: Proxy smallOrder) tst k
   getAllBounds = undefined
 
 -- | Go down the tables until we find the first table for our big order.
@@ -160,22 +194,22 @@ instance
   ) ⇒ ThisBigOrder n False (ts:.t) where
   {-# Inline thisBigOrder #-}
   thisBigOrder Proxy Proxy (ts:.t) =
-    thisBigOrder (Proxy ∷ Proxy n) (Proxy ∷ Proxy (IsThisBigOrder n ts)) ts
+    thisBigOrder (Proxy :: Proxy n) (Proxy :: Proxy (IsThisBigOrder n ts)) ts
   getAllBounds = undefined
 
 -- |
 
-class EachSmallOrder (bigOrder ∷ Nat) (smallOrders ∷ [Nat]) ts i where
+class EachSmallOrder (bigOrder :: Nat) (smallOrders :: [Nat]) ts i where
   eachSmallOrder
-    ∷ Proxy bigOrder
+    :: Proxy bigOrder
     -- ^ Only fill exactly this big order
-    → Proxy smallOrders
+    -> Proxy smallOrders
     -- ^ These are all the small order to go through.
-    → ts
+    -> ts
     -- ^ set of tables.
-    → i
+    -> i
     -- ^ index to update.
-    → ST s ()
+    -> ST s ()
 
 -- | Went through all tables, nothing more to do.
 
@@ -195,14 +229,14 @@ instance
   {-# Inline eachSmallOrder #-}
   eachSmallOrder Proxy Proxy ts i = do
     -- fill all tables that have the same big & small order
-    thisSmallOrder (Proxy ∷ Proxy bigOrder) (Proxy ∷ Proxy s) (Proxy ∷ Proxy isThisOrder) ts i
+    thisSmallOrder (Proxy :: Proxy bigOrder) (Proxy :: Proxy s) (Proxy :: Proxy isThisOrder) ts i
     -- fill tables with the next small order
-    eachSmallOrder (Proxy ∷ Proxy bigOrder) (Proxy ∷ Proxy so) ts i
+    eachSmallOrder (Proxy :: Proxy bigOrder) (Proxy :: Proxy so) ts i
 
 -- |
 
-class ThisSmallOrder (bigNat ∷ Nat) (smallNat ∷ Nat) (thisOrder ∷ Bool) ts i where
-  thisSmallOrder ∷ Proxy bigNat → Proxy smallNat → Proxy thisOrder → ts → i → ST s ()
+class ThisSmallOrder (bigNat :: Nat) (smallNat :: Nat) (thisOrder :: Bool) ts i where
+  thisSmallOrder :: Proxy bigNat -> Proxy smallNat -> Proxy thisOrder -> ts -> i -> ST s ()
 
 instance ThisSmallOrder b s any Z i where
   {-# Inline thisSmallOrder #-}
@@ -215,8 +249,8 @@ instance
   , ThisSmallOrder bigOrder smallOrder isThisOrder ts i
   ) ⇒ ThisSmallOrder bigOrder smallOrder 'False (ts:.t) i where
   {-# Inline thisSmallOrder #-}
-  thisSmallOrder Proxy Proxy Proxy (ts:.t) i =
-    thisSmallOrder (Proxy ∷ Proxy bigOrder) (Proxy ∷ Proxy smallOrder) (Proxy ∷ Proxy isThisOrder) ts i
+  -- TODO eta-reduced, does this destroy performance?
+  thisSmallOrder Proxy Proxy Proxy (ts:.t) = thisSmallOrder (Proxy :: Proxy bigOrder) (Proxy :: Proxy smallOrder) (Proxy :: Proxy isThisOrder) ts
 
 -- | This instance fills only dense tables. Sparse tables will use a slightly different system,
 -- where writes may fail silently!
@@ -229,7 +263,7 @@ instance
   , isThisSmallOrder ~ IsThisSmallOrder smallOrder ts
   , isThisOrder ~ (isThisBigOrder && isThisSmallOrder)
   , ThisSmallOrder bigOrder smallOrder isThisOrder ts i
-  ) ⇒ ThisSmallOrder bigOrder smallOrder 'True (ts:.TwITbl bo so Id (Dense v) c i x) i where
+  ) => ThisSmallOrder bigOrder smallOrder 'True (ts:.TwITbl bo so Id (Dense v) c i x) i where
   {-# Inline thisSmallOrder #-}
   thisSmallOrder Proxy Proxy Proxy (ts:.TW (ITbl _ arr) f) i = do
     let uB = upperBound arr
@@ -237,7 +271,7 @@ instance
     z <- return . unId $ inline f uB i
     writeM marr i z
     -- TODO need to write test case that checks that all tables are always filled
-    thisSmallOrder (Proxy ∷ Proxy bigOrder) (Proxy ∷ Proxy smallOrder) (Proxy ∷ Proxy isThisOrder) ts i
+    thisSmallOrder (Proxy :: Proxy bigOrder) (Proxy :: Proxy smallOrder) (Proxy :: Proxy isThisOrder) ts i
 
 instance
   ( PrimArrayOps (PAS.Sparse ixw v) i x
@@ -255,10 +289,10 @@ instance
   thisSmallOrder Proxy Proxy Proxy (ts:.TW (ITbl _ arr) f) i = do
     let uB = upperBound arr
     marr <- unsafeThawM arr
-    z ← return . unId $ inline f uB i
+    z <- return . unId $ inline f uB i
     safeWriteM marr i z
     -- TODO need to write test case that checks that all tables are always filled
-    thisSmallOrder (Proxy ∷ Proxy bigOrder) (Proxy ∷ Proxy smallOrder) (Proxy ∷ Proxy isThisOrder) ts i
+    thisSmallOrder (Proxy :: Proxy bigOrder) (Proxy :: Proxy smallOrder) (Proxy :: Proxy isThisOrder) ts i
 
 -- | This is a mixed instance for MCFG's. Beware of the mixture of dimensions here! Right now, this
 -- is only for testing, check how the outer index structure is "larger" than the inner index
@@ -285,7 +319,7 @@ instance
       marr <- unsafeThawM arr
       z <- return . unId $ inline f uB low
       safeWriteM marr low z
-    thisSmallOrder (Proxy ∷ Proxy bigOrder) (Proxy ∷ Proxy smallOrder) (Proxy ∷ Proxy isThisOrder) ts i
+    thisSmallOrder (Proxy :: Proxy bigOrder) (Proxy :: Proxy smallOrder) (Proxy :: Proxy isThisOrder) ts i
 
 
 
@@ -295,7 +329,7 @@ instance
 
 type BigOrderNats arr = Nub (Sort (BigOrderNats' arr))
 
-type family BigOrderNats' arr ∷ [Nat]
+type family BigOrderNats' arr :: [Nat]
 
 type instance BigOrderNats' Z = '[]
 
@@ -303,7 +337,7 @@ type instance BigOrderNats' (ts:.TwITbl bo so m arr c i x) = bo ': BigOrderNats'
 
 
 
-type family IsThisBigOrder (n ∷ Nat) arr ∷ Bool
+type family IsThisBigOrder (n :: Nat) arr :: Bool
 
 type instance IsThisBigOrder n Z = 'False
 
@@ -313,7 +347,7 @@ type instance IsThisBigOrder n (ts:.TwITbl bo so m arr c i x) = n == bo
 
 type SmallOrderNats arr = Nub (Sort (SmallOrderNats' arr))
 
-type family SmallOrderNats' arr ∷ [Nat]
+type family SmallOrderNats' arr :: [Nat]
 
 type instance SmallOrderNats' Z = '[]
 
@@ -323,7 +357,7 @@ type instance SmallOrderNats' (ts:.TwITbl bo so m arr c i x) = so ': SmallOrderN
 
 
 
-type family IsThisSmallOrder (n ∷ Nat) arr ∷ Bool
+type family IsThisSmallOrder (n :: Nat) arr :: Bool
 
 type instance IsThisSmallOrder n Z = 'False
 
@@ -349,11 +383,11 @@ data PerfCounter = PerfCounter
 
 instance NFData PerfCounter
 
-showPerfCounter ∷ PerfCounter → String
+showPerfCounter :: PerfCounter -> String
 {-# NoInline showPerfCounter #-}
 showPerfCounter PerfCounter{..} =
   let cellsSecond = round $ fromIntegral numberOfCells / seconds
-      m ∷ Integer = 1000000
+      m :: Integer = 1000000
   in  printf "%.4f seconds, %d,%06d cells @ %d,%06d cells/second"
              seconds
              (numberOfCells `div` m) (numberOfCells `mod` m)
@@ -365,8 +399,8 @@ instance Semigroup PerfCounter where
   PerfCounter p1 s1 n1 <> PerfCounter p2 s2 n2 = PerfCounter (p1+p2) (s1+s2) (n1+n2)
 
 
-class CountNumberOfCells (n ∷ Nat) t where
-  countNumberOfCells ∷ Maybe (Proxy n) → t → Integer
+class CountNumberOfCells (n :: Nat) t where
+  countNumberOfCells :: Maybe (Proxy n) -> t -> Integer
 
 instance CountNumberOfCells n Z where
   {-# NoInline countNumberOfCells #-}
@@ -381,11 +415,11 @@ instance
   ) ⇒ CountNumberOfCells n (ts:.TwITbl bo so Id arr c i x) where
   {-# NoInline countNumberOfCells #-}
   countNumberOfCells mayP (ts:.(TW (ITbl _ arr) fun)) =
-    let n  = natVal (Proxy ∷ Proxy n)
-        bo = natVal (Proxy ∷ Proxy bo)
+    let n  = natVal (Proxy :: Proxy n)
+        bo = natVal (Proxy :: Proxy bo)
         cs = countNumberOfCells mayP ts
         c  = product . totalSize $ upperBound arr
     in  case mayP of
-      Nothing → cs + c
-      Just _  → cs + if n==bo then c else 0
+      Nothing -> cs + c
+      Just _  -> cs + if n==bo then c else 0
 
